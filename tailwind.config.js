b9d6497e97d79c671d8011a6c70a74815d66bb3c
YazidLawliette <yzeeadddd@gmail.com>
2024-09-03 19:24:11 +0700

    fifchange

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      "colors": {
+        'navbar-bg': '#1B5BDF',
+      },
       "space": {
         "13": "width: 8px"
       },
